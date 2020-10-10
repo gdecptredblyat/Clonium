4c6c847a79823f01404da99acb76ae7f31bf0adf
Dmitry <grashin2011@gmail.com>
2020-10-10 23:53:58 +0300

    changed logic of the game

--- games/gamers_visualise.py
+++ games/gamers_visualise.py
@@ -33,13 +33,6 @@ class CField:
         self.t = 1
 
 
-AList = []
-Previous_waves = []
-DList_1 = []
-DList_2 = []
-Users = []
-Former_users = []
-
 def Print_Field():
     for f in range(length):
         st = []
@@ -133,7 +126,7 @@ def pick_color(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
         # pixel = image_hsv[y,x]
         start = 1
-        print(x,y)
+        # print(x,y)
         give_x_y(x,y)
 
 
@@ -152,7 +145,8 @@ def Add_count(i, j):
 def Clear_Dis_List():
 
     for c in range(len(Main_DList)):
-        Disclosure(Main_DList[0][0], Main_DList[0][1])
+        result = Disclosure(Main_DList[0][0], Main_DList[0][1])
+        print(result)
         Main_DList.pop(0)
 
     if len(AList) > 0:
@@ -160,38 +154,24 @@ def Clear_Dis_List():
 
 
 def Clear_Add_List():
-    # if (Field.t == 1):
-    #     Main_DList = DList_1
-    # else:
-    #     Main_DList = DList_2
-
-
     for c in range(len(AList)):
         Add_count(AList[0][0], AList[0][1])
         AList.pop(0)
-    # Print_Field()
-    # cv_image = Visualise_Field()
-
 
     Clear_Dis_List()
-    # if (Field.t == 1):
-    #     Main_DList = DList_2
-    #     Field.t = 2
-    # else:
-    #     Main_DList = DList_1
-    #     Field.t = 1
-
 
 def Disclosure(i, j):
 
-    Visual_Disclosure(i,j)
     Field.items[i][j].k -= 4
+    if Field.items[i][j].k < 0:
+        Field.items[i][j].k += 4
+        return False
 
-
-    if (Field.items[i][j].k ==0):
+    Visual_Disclosure(i,j)
+    if Field.items[i][j].k == 0:
         Field.items[i][j].color = 0
+        Main_User.number_of_cells-=1
     Main_User.sum-=4
-    Main_User.number_of_cells-=1
     for c in [-1, 1]:
         if (i + c < length) and (i + c >= 0):
             AList.append([i + c, j])
@@ -212,6 +192,13 @@ def Disclosure(i, j):
                     Main_User.sum += Field.items[i ][j+c].k
                     Users[n].sum -= Field.items[i ][j+c].k
             Field.items[i][j+c].color = Main_User.allow
+    return True
+AList = []
+Previous_waves = []
+DList_1 = []
+
+Users = []
+Former_users = []
 
 length = int(input("Enter length: "))
 width = int(input("Enter width: "))
@@ -222,13 +209,11 @@ Main_DList = DList_1
 start = [1,1,1,width -2 ,length -2 , 1,length-2,width -2 ]
 
 
-number_of_gamers = int(input("Enter number of gamers"))
+number_of_gamers = int(input("Enter number of gamers: "))
 for n in range(number_of_gamers):
     Users.append(Gamer(n+1,3,1,start[2*n],start[2*n+1]))
     Field.items[start[2*n]][start[2*n+1]].k = 3
     Field.items[start[2 * n]][start[2 * n + 1]].color = Users[n].allow
-
-
 
 player_error=True
 Game = 1
@@ -237,8 +222,9 @@ wave = 1
 
 start = 0
 
-# Print_Field()
+
 Visualise_Field()
+
 while Game:
     for n in range(number_of_gamers):
         if Users[n].sum != 0 and len(Former_users) < number_of_gamers-1:
@@ -279,6 +265,3 @@ while Game:
                 print("Game over. Gamer",n+1," won!!!")
         Previous_waves.append(Field.items)
     wave+=1
-
-# print(*Previous_waves)
-#можно потом будет убрать чтобы ставилось обязательно только на свои cells
